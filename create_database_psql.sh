669e7fccf8c38d55f2eaa5345fd6f25435c950d5
IX <borntokill.ix@gmail.com>
2025-09-16 21:43:48 +0700

    build(create_database_psql.sh): add bash script for create user universal with database universal

---
+++ create_database_psql.sh
@@ -0,0 +1,20 @@
+PASSWORD=$(head /dev/random | sha256sum | grep -Po '\w+')
+USER="universal"
+DATABASE="universal"
+echo "Create user $USER with password $PASSWORD" | tee .create_user.log
+sudo -u postgres psql -c "CREATE ROLE $USER WITH LOGIN PASSWORD '$PASSWORD' CREATEDB CREATEROLE;" 
+sudo -u postgres psql -c "CREATE DATABASE $DATABASE OWNER $USER;"
+
+cat app/core/database/schema.sql | sudo psql -U $USER
+echo
+sudo psql -U $USER -c "SELECT pg_size_pretty(pg_table_size(core.oid)) AS size_tables,
+        pg_size_pretty(pg_indexes_size(core.oid)) AS size_index,
+        pg_size_pretty(pg_total_relation_size(core.oid)) AS total_size,
+        core.relname AS table
+FROM pg_class core
+    LEFT JOIN pg_namespace space
+        ON core.relnamespace=space.oid
+WHERE space.nspname='public' AND core.relkind='r'
+ORDER BY pg_total_relation_size(core.oid) DESC
+LIMIT 20;"
+
