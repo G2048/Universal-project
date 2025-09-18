29dfba5b8ff8375fc4cad49ad1656a72467bd45d
IX <borntokill.ix@gmail.com>
2025-09-18 22:32:28 +0700

    feat(app/api/services/jwt.py): add class JWT for generation of jwt token; JwtRefreshTokenPayloadfor refresh token; JwtPayload for description of jwt token

---
+++ app/api/services/jwt.py
@@ -0,0 +1,59 @@
+from datetime import datetime, timedelta
+from uuid import uuid4
+
+import jwt
+from jwt import ExpiredSignatureError, InvalidTokenError
+from pydantic import BaseModel, ConfigDict, Field, computed_field
+
+from app.configs.settings import JwtSettings
+
+
+def uuid_str_factory() -> str:
+    return str(uuid4())
+
+
+class JwtPayload(BaseModel):
+    model_config = ConfigDict(extra="allow")
+
+    sub: str
+    ttl: int = 900 * 60 * 24 * 30  # 30 days
+    jti: str = Field(default_factory=uuid_str_factory)
+
+    @computed_field(return_type=int)
+    def exp(self):
+        expiration_time = datetime.now() + timedelta(seconds=self.ttl)
+        return int(expiration_time.timestamp())
+
+    @computed_field(return_type=int)
+    def iat(self):
+        return int(datetime.now().timestamp())
+
+
+class JwtRefreshTokenPayload(JwtPayload):
+    version: int
+    user_agent: str | None = "curl"
+    ttl: int = 30 * 24 * 60 * 60  # 30 days in seconds
+
+
+class JWT:
+    _instance = None
+    _jwt_settings = JwtSettings()
+    SECRET_KEY = _jwt_settings.secret_key
+    ALGORITHM = _jwt_settings.algorithm
+
+    @classmethod
+    def generate_token(cls, payload: JwtPayload) -> str:
+        return jwt.encode(payload.model_dump(), cls.SECRET_KEY, algorithm=cls.ALGORITHM)
+
+    @classmethod
+    def validate(cls, token: str) -> JwtPayload:
+        _jwt = jwt.decode(token, cls.SECRET_KEY, algorithms=[cls.ALGORITHM])
+        return JwtPayload(**_jwt)
+
+    @classmethod
+    def payload(cls, token: str) -> JwtPayload:
+        _jwt = jwt.decode(token, options={"verify_signature": False})
+        return JwtPayload(**_jwt)
+
+
+__all__ = ("JWT", "ExpiredSignatureError", "InvalidTokenError", "JwtPayload")
