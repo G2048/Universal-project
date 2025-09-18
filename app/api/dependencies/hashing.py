d330d5f907d95d98b014176df6fcfe3c2b520bd8
IX <borntokill.ix@gmail.com>
2025-09-18 22:07:34 +0700

    refactor(app/api/dependencies/hashing.py): change hash function in hash_password()

--- app/api/dependencies/hashing.py
+++ app/api/dependencies/hashing.py
@@ -1,5 +1,12 @@
 import hashlib
+import os
+
+salt = os.environ.get("SALT")
+if not salt:
+    salt = "test_salt"
 
 
 def hash_password(password: str) -> str:
-    return hashlib.md5(password.encode("utf-8")).hexdigest()
+    return hashlib.pbkdf2_hmac(
+        "sha256", password.encode("utf-8"), salt.encode("utf-8"), 100000
+    ).hex()
