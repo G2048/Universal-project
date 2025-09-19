9bfa90534d623a12bfcb62a24428f676613db979
IX <borntokill.ix@gmail.com>
2025-09-19 12:19:28 +0700

    feat(app/api/v1/routers/companies.py): add check user ACL

--- app/api/v1/routers/companies.py
+++ app/api/v1/routers/companies.py
@@ -4,11 +4,27 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import Response
 from sqlmodel import select
 
-from app.api.dependencies.db import Session, get_db_connection
+from app.api.dependencies import (
+    Scope,
+    Session,
+    check_permissions,
+    get_db_connection,
+    validate_user,
+)
 from app.api.models.companies import CreateCompany
 from app.core.database.models import Companies, UserGroups
 
-router = APIRouter(prefix="/companies", tags=["Companies"])
+
+def _check_permissions(
+    user_id: int = Depends(validate_user),
+    session: Session = Depends(get_db_connection),
+):
+    return check_permissions(user_id, Scope.companies, session)
+
+
+router = APIRouter(
+    prefix="/companies", tags=["Companies"], dependencies=[Depends(_check_permissions)]
+)
 logger = logging.getLogger("app.api.v1.routers.companies")
 
 
