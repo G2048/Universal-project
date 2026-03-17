8385564f7d136fde5440717061e1e312cd9aeb0e
IX <borntokill.ix@gmail.com>
2026-03-18 01:19:40 +0700

    build(database): move schema.sql of database to build folder for automatin creation and fill database in first docker start up

--- build/create_database_psql.sh
+++ build/create_database_psql.sh
@@ -2,14 +2,16 @@ PASSWORD=$(head /dev/random | sha256sum | grep -Po '\w+')
 USER="universal"
 DATABASE="universal"
 echo "Create user USER=$USER with password PASSWORD=$PASSWORD" | tee .create_user.log
-sudo -u postgres psql -c "CREATE ROLE $USER WITH LOGIN PASSWORD '$PASSWORD' CREATEDB CREATEROLE;"
-sudo -u postgres psql -c "CREATE DATABASE $DATABASE OWNER $USER;"
-sudo -u postgres psql -c "CREATE SEQUENCE IF NOT EXISTS user_roles_id_seq OWNED BY user_roles.id;"
-sudo -u postgres psql -c "ALTER TABLE user_roles ALTER COLUMN id SET DEFAULT nextval('user_roles_id_seq');"
+psql -c "CREATE ROLE $USER WITH LOGIN PASSWORD '$PASSWORD' CREATEDB CREATEROLE;"
+psql -c "CREATE DATABASE $DATABASE OWNER $USER;"
 
-cat app/core/database/schema.sql | sudo psql -U $USER
+cat schema.sql | psql -U $USER
+
+psql -c "CREATE SEQUENCE IF NOT EXISTS user_roles_id_seq OWNED BY user_roles.id;"
+psql -c "ALTER TABLE user_roles ALTER COLUMN id SET DEFAULT nextval('user_roles_id_seq');"
+
 echo
-sudo psql -U $USER -c "SELECT pg_size_pretty(pg_table_size(core.oid)) AS size_tables,
+psql -U $USER -c "SELECT pg_size_pretty(pg_table_size(core.oid)) AS size_tables,
         pg_size_pretty(pg_indexes_size(core.oid)) AS size_index,
         pg_size_pretty(pg_total_relation_size(core.oid)) AS total_size,
         core.relname AS table
